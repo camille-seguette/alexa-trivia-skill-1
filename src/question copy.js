3a58cd96819ad0beb6e1ba738026d5af5fa515ad
Camille <camilleseguette@gmail.com>
2017-11-07 15:13:59 -0800

    Alexa Silly Sandwich Quiz function attempts

---
+++ src/question copy.js
@@ -0,0 +1,218 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_GB: [
+        {
+            'Which ingredient does not come on a Chicago style hot dog?': [
+                'Ketchup',
+                'Mustard',
+                'Onion',
+                'Sweet pickle relish',
+                'Dill pickle spear',
+                'Tomato',
+                'Pickled sport peppers',
+                'Celery salt',
+            ],
+        },
+        {
+            'What is the difference between a Croque Monsieur and Croque Madame?': [
+                'The Croque Madame has a fried egg on top.',
+                'The Croque Monsieur has a fried egg on top.',
+                'The Croque Madame has Béchamel sauce.',
+                'The Croque Monsieur has Béchamel sauce.',
+                'The Croque Madame has grilled ham instead of boiled ham.',
+                'The Croque Monsieur has grilled ham instead of boiled ham.',
+            ],
+        },
+        {
+            'What kind of cheese is the Philly Cheesesteak originally made with?': [
+                'Provolone',
+                'Swiss',
+                'Meunster',
+                'White Cheddar',
+                'Cheez Whiz',
+            ],
+        },
+        {
+            'What is the base of a jibarito sandwich?': [
+                'Fried plantains',
+                'Bollo roll',
+                'Baguette',
+                'Tortilla',
+                'Challah roll',
+                'Sourdough',
+                'Lettuce',
+            ],
+        },
+        {
+            'What is the defining ingredient in the Italian Beef sandwich?': [
+                'Giardiñiera',
+                'Roasted red peppers',
+                'Green peppers and onion',
+                'Beef juice',
+                'Beef',
+                'Cheese',
+                'Egg',
+            ],
+        },
+        {
+            'In addition to meat, the Vietnamese bánh mì sandwich traditionally comes with:': [
+                'Carrots, cucumber, daikon, and jalapeño',
+                'Carrots, jicama, and lettuce',
+                'Carrots, daikon, and pickles',
+                'Carrots, cucumber, and cabbage',
+            ],
+        },
+        {
+            'Where is the origin of the jibarito?': [
+                'Chicago',
+                'New York',
+                'Puerto Rico',
+                'Colombia',
+                'Los Angeles',
+                'Cuba',
+            ],
+        },
+        {
+            'Who was running for president when they ordered Swiss cheese on their Philly Cheesesteak, which is considered to be a major faux pas?': [
+                'John Kerry',
+                'Barack Obama',
+                'George Bush',
+                'Bill Clinton',
+            ],
+        },
+        {
+            'Alongside ham and pork, a Cubano sandwich contains what ingredients?': [
+                'Swiss cheese, pickles, and mustard',
+                'Swiss cheese, green peppers, and mustard',
+                'Meunster cheese, pickles, and mustard',
+                'Muenster cheese, green peppers, and mustard',
+            ],
+        },
+        {
+            'The Elvis sandwich is comprised of:': [
+                'Peanut butter, banana, and bacon',
+                'Peanut butter, honey, and bacon',
+                'Peanut butter, banana, and honey',
+                'Peanut butter, marshmallow fluff, and bacon',
+            ],
+        },
+        {
+            'The 3 main ingredients in the Chilean Lomito Italiano, named after the colors of the Italian flag, are:': [
+                'Tomato, mayo, and avocado',
+                'Tomato, mayo, and lettuce',
+                'Roasted red peppers, garlic aioli, and avocado',
+                'Beet, goat cheese, and lettuce',
+            ],
+        },
+    ],
+    QUESTIONS_EN_US: [
+        {
+            'Which ingredient does not come on a Chicago style hot dog?': [
+                'Ketchup',
+                'Mustard',
+                'Onion',
+                'Sweet pickle relish',
+                'Dill pickle spear',
+                'Tomato',
+                'Pickled sport peppers',
+                'Celery salt',
+            ],
+        },
+        {
+            'What is the difference between a Croque Monsieur and Croque Madame?': [
+                'The Croque Madame has a fried egg on top.',
+                'The Croque Monsieur has a fried egg on top.',
+                'The Croque Madame has Béchamel sauce.',
+                'The Croque Monsieur has Béchamel sauce.',
+                'The Croque Madame has grilled ham instead of boiled ham.',
+                'The Croque Monsieur has grilled ham instead of boiled ham.',
+            ],
+        },
+        {
+            'What kind of cheese is the Philly Cheesesteak originally made with?': [
+                'Provolone',
+                'Swiss',
+                'Meunster',
+                'White Cheddar',
+                'Cheez Whiz',
+            ],
+        },
+        {
+            'What is the base of a jibarito sandwich?': [
+                'Fried plantains',
+                'Bollo roll',
+                'Baguette',
+                'Tortilla',
+                'Challah roll',
+                'Sourdough',
+                'Lettuce',
+            ],
+        },
+        {
+            'What is the defining ingredient in the Italian Beef sandwich?': [
+                'Giardiñiera',
+                'Roasted red peppers',
+                'Green peppers and onion',
+                'Beef juice',
+                'Beef',
+                'Cheese',
+                'Egg',
+            ],
+        },
+        {
+            'In addition to meat, the Vietnamese bánh mì sandwich traditionally comes with:': [
+                'Carrots, cucumber, daikon, and jalapeño',
+                'Carrots, jicama, and lettuce',
+                'Carrots, daikon, and pickles',
+                'Carrots, cucumber, and cabbage',
+            ],
+        },
+        {
+            'Where is the origin of the jibarito?': [
+                'Chicago',
+                'New York',
+                'Puerto Rico',
+                'Colombia',
+                'Los Angeles',
+                'Cuba',
+            ],
+        },
+        {
+            'Who was running for president when they ordered Swiss cheese on their Philly Cheesesteak, which is considered to be a major faux pas?': [
+                'John Kerry',
+                'Barack Obama',
+                'George Bush',
+                'Bill Clinton',
+            ],
+        },
+        {
+            'Alongside ham and pork, a Cubano sandwich contains what ingredients?': [
+                'Swiss cheese, pickles, and mustard',
+                'Swiss cheese, green peppers, and mustard',
+                'Meunster cheese, pickles, and mustard',
+                'Muenster cheese, green peppers, and mustard',
+            ],
+        },
+        {
+            'The Elvis sandwich is comprised of:': [
+                'Peanut butter, banana, and bacon',
+                'Peanut butter, honey, and bacon',
+                'Peanut butter, banana, and honey',
+                'Peanut butter, marshmallow fluff, and bacon',
+            ],
+        },
+        {
+            'The 3 main ingredients in the Chilean Lomito Italiano, named after the colors of the Italian flag, are:': [
+                'Tomato, mayo, and avocado',
+                'Tomato, mayo, and lettuce',
+                'Roasted red peppers, garlic aioli, and avocado',
+                'Beet, goat cheese, and lettuce',
+            ],
+        },
+    ],
+};
